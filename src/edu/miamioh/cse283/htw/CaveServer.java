0cc6a5db933de8b227b20a0510452ed0e1f09f14
dazeycm <dazeycm@miamioh.edu>
2014-11-17 23:06:17 -0500

    Added ability to move between rooms

--- src/edu/miamioh/cse283/htw/CaveServer.java
+++ src/edu/miamioh/cse283/htw/CaveServer.java
@@ -4,14 +4,15 @@ import java.net.*;
 import java.util.*;
 
 /**
-* The CaveServer class takes the following command-line parameters:
-* 
-* <Hostname of CaveSystemServer> <port number of CaveSystemServer> <port number of this CaveServer>
-* 
-* E.g., "localhost 1234 2000" 
-*/
+ * The CaveServer class takes the following command-line parameters:
+ * 
+ * <Hostname of CaveSystemServer> <port number of CaveSystemServer> <port number
+ * of this CaveServer>
+ * 
+ * E.g., "localhost 1234 2000"
+ */
 public class CaveServer {
-	
+
 	/** Port base for this cave server. */
 	protected int portBase;
 
@@ -20,51 +21,55 @@ public class CaveServer {
 
 	/** Proxy to the CaveSystemServer. */
 	protected CaveSystemServerProxy caveSystem;
-		
+
 	/** Rooms in this CaveServer. */
 	protected ArrayList<Room> rooms;
-	
+
 	/** Constructor. */
 	public CaveServer(CaveSystemServerProxy caveSystem, int portBase) {
 		this.caveSystem = caveSystem;
 		this.portBase = portBase;
-		
+
 		rooms = new ArrayList<Room>();
 		rooms.add(new Room(0));
 		rooms.add(new Room(1));
 		rooms.add(new Room(2));
 		rooms.get(0).neighbors.add(rooms.get(1));
-		
+
 		rooms.get(1).neighbors.add(rooms.get(0));
 		rooms.get(1).neighbors.add(rooms.get(2));
-		
+
 		rooms.get(2).neighbors.add(rooms.get(1));
-		
-		for(int i=3; i<20; ++i) {
+
+		for (int i = 3; i < 20; ++i) {
 			Room room = new Room(i);
-			//room.roomNum = i;
+			// room.roomNum = i;
 			Random r = new Random();
 			rooms.add(room);
-			if (i == 3)	{
+			if (i == 3) {
 				rooms.get(2).neighbors.add(rooms.get(3));
-				for(int j=0; j < 2; j++)	{
-					room.neighbors.add(rooms.get(r.nextInt(rooms.size())));	//add random neighbors
+				for (int j = 0; j < 2; j++) {
+					room.neighbors.add(rooms.get(r.nextInt(rooms.size()))); // add random neighbors
 				}
-			}
-			else{	
-				for(int j=0; j < 3; j++)	{
-					room.neighbors.add(rooms.get(r.nextInt(rooms.size())));	//add random neighbors
+			} else {
+				for (int j = 0; j < 3; j++) {
+					room.neighbors.add(rooms.get(r.nextInt(rooms.size()))); // add  random neighbors
 				}
 			}
 		}
 	}
 
 	/** Returns the port number to use for accepting client connections. */
-	public int getClientPort() { return portBase; }
+	public int getClientPort() {
+		return portBase;
+	}
 
 	/** This is the thread that handles a single client connection. */
 	public class ClientThread implements Runnable {
-		/** This is our "client" (actually, a proxy to the network-connected client). */
+		/**
+		 * This is our "client" (actually, a proxy to the network-connected
+		 * client).
+		 */
 		protected ClientProxy client;
 
 		/** Constructor. */
@@ -72,32 +77,37 @@ public class CaveServer {
 			this.client = client;
 		}
 
-		/** Play the game with this client.
+		/**
+		 * Play the game with this client.
 		 */
 		public void run() {
 			try {
 				client.message("Abandon all hope ye who enter here!");
 				// put the player in a room (any room is fine)
 				rooms.get(0).players.add(client);
-				while(true)	{
-					//int oldRoom = 
+				int curRoom = 0;
+				int newRoom = 0;
+				while (true) {
+
 					client.senses("You are in an empty room.");
-					client.message("The connecting rooms are " + rooms.get(0).printNeighbors());	
+					client.message("The connecting rooms are "
+							+ rooms.get(curRoom).printNeighbors());
 					String action = client.getAction();
-					
-					if(action.contains("move"))	{
+
+					if (action.contains("move")) {
 						action = action.replaceAll("[^-?0-9]", "");
-						int roomNum = Integer.parseInt(action);
+						newRoom = Integer.parseInt(action);
+						rooms.get(curRoom).players.remove(client);
+						rooms.get(newRoom).players.add(client);
+						curRoom = newRoom;
 					}
-					
 					
 					// -- and retrieve their action:
 					// -- and perform the action
-					//client.message(action);
+					// client.message(action);
 				}
-				
-				
-			} catch(Exception ex) {
+
+			} catch (Exception ex) {
 				// If an exception is thrown, we can't fix it here -- Crash.
 				ex.printStackTrace();
 				System.exit(1);
@@ -111,14 +121,14 @@ public class CaveServer {
 			clientSocket = new ServerSocket(getClientPort());
 			caveSystem.register(clientSocket);
 
-			while(true) {
+			while (true) {
 				// and now loop forever, accepting client connections:
-				while(true) {
+				while (true) {
 					ClientProxy client = new ClientProxy(clientSocket.accept());
 					(new Thread(new ClientThread(client))).start();
 				}
 			}
-		} catch(Exception ex) {
+		} catch (Exception ex) {
 			// If an exception is thrown, we can't fix it here -- Crash.
 			ex.printStackTrace();
 			System.exit(1);
@@ -127,19 +137,20 @@ public class CaveServer {
 
 	/** Main method (run the CaveServer). */
 	public static void main(String[] args) throws Exception {
-		InetAddress addr=InetAddress.getByName("localhost");
-		int cssPortBase=1234;
-		int cavePortBase=2000;
-		
-		if(args.length > 0) {
+		InetAddress addr = InetAddress.getByName("localhost");
+		int cssPortBase = 1234;
+		int cavePortBase = 2000;
+
+		if (args.length > 0) {
 			addr = InetAddress.getByName(args[0]);
 			cssPortBase = Integer.parseInt(args[1]);
 			cavePortBase = Integer.parseInt(args[2]);
 		}
-		
+
 		// first, we need our proxy object to the CaveSystemServer:
-		CaveSystemServerProxy caveSystem = new CaveSystemServerProxy(new Socket(addr, cssPortBase+1));
-		
+		CaveSystemServerProxy caveSystem = new CaveSystemServerProxy(
+				new Socket(addr, cssPortBase + 1));
+
 		// now construct this cave server, and run it:
 		CaveServer cs = new CaveServer(caveSystem, cavePortBase);
 		cs.run();
