9022b9cd5a27218816c5ecba92d27a2ce42ef31a
Craig Dazey <dazeycm@miamioh.edu>
2014-11-18 23:35:51 -0500

    Fixed cave neighbor output

--- src/edu/miamioh/cse283/htw/Room.java
+++ src/edu/miamioh/cse283/htw/Room.java
@@ -6,6 +6,9 @@ public class Room {
 	public ArrayList<ClientProxy> players;
 	public ArrayList<Room> neighbors;
 	public int roomNum;
+	boolean hasBats;
+	boolean hasWumpus;
+	boolean hasPit;
 	
 	public Room(int roomNum) {
 		players = new ArrayList<ClientProxy>();
@@ -20,7 +23,26 @@ public class Room {
 	public String printNeighbors()	{
 		String ret = "";
 		for(int i = 0; i < getNumNeighbors(); i++){
-			ret += this.neighbors.get(i).roomNum + ", ";
+			if(getNumNeighbors() == 1)
+			ret += this.neighbors.get(i).roomNum;
+			
+			if(getNumNeighbors() == 2)	{
+				if (i == 0)	{
+					ret += this.neighbors.get(i).roomNum + " and, ";
+				}
+				if (i == 1)	{
+					ret += this.neighbors.get(i).roomNum;
+				}
+			}
+			
+			if(getNumNeighbors() == 3)	{
+				if (i == 0 || i == 1)	{
+					ret += this.neighbors.get(i).roomNum + ", ";
+				}
+				if (i == 2)
+					ret += "and " + this.neighbors.get(i).roomNum; 
+			}
+			
 		}
 		return ret;
 	}
